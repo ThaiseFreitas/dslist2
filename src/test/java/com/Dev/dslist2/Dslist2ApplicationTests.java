3a474f7e6e221789b09b6ca416337add67a4974b
Thaise freitas <freitasthaise1@gmail.com>
2024-11-13 10:02:54 -0300

    projeto created

---
+++ src/test/java/com/Dev/dslist2/Dslist2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dev.dslist2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslist2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
